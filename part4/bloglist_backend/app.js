2f8a10f1e162dd08663a0ba9c0c5971674a662ab
mauro <mauro.castermans@gmail.com>
2023-09-20 21:37:19 +0200

    part 4a completed

---
+++ part4/bloglist_backend/app.js
@@ -0,0 +1,34 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+morgan.token("body", (req) => JSON.stringify(req.body));
+
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
---
+++ part4/bloglist_backend/utils/list_helper.js
@@ -0,0 +1,62 @@
+const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {};
+  else {
+    const blogWithMaxLikes = blogs.reduce(
+      (blogWithMaxLikes, blog) =>
+        blog.likes > blogWithMaxLikes.likes ? blog : blogWithMaxLikes,
+      { likes: 0 }
+    );
+    return {
+      title: blogWithMaxLikes.title,
+      author: blogWithMaxLikes.author,
+      likes: blogWithMaxLikes.likes,
+    };
+  }
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return {};
+
+  const authorsWithNumberOfBlogs = {};
+  blogs.forEach((blog) => {
+    if (authorsWithNumberOfBlogs[blog.author])
+      authorsWithNumberOfBlogs[blog.author]++;
+    else authorsWithNumberOfBlogs[blog.author] = 1;
+  });
+
+  const topAuthor = Object.keys(authorsWithNumberOfBlogs).reduce(
+    (topAuthor, author) =>
+      authorsWithNumberOfBlogs[topAuthor] > authorsWithNumberOfBlogs[author]
+        ? topAuthor
+        : author
+  );
+  return { author: topAuthor, blogs: authorsWithNumberOfBlogs[topAuthor] };
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return {};
+
+  const authorsWithNumberOfLikes = {};
+  blogs.forEach((blog) => {
+    if (authorsWithNumberOfLikes[blog.author])
+      authorsWithNumberOfLikes[blog.author] += blog.likes;
+    else authorsWithNumberOfLikes[blog.author] = blog.likes;
+  });
+
+const topAuthor = Object.keys(authorsWithNumberOfLikes).reduce(
+  (topAuthor, author) =>
+    authorsWithNumberOfLikes[topAuthor] > authorsWithNumberOfLikes[author]
+      ? topAuthor
+      : author
+);
+return { author: topAuthor, likes: authorsWithNumberOfLikes[topAuthor] };
+};
+
+module.exports = {
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+};
