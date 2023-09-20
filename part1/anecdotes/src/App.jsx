255f31ea551d4e94a7076bd962b6f20600fd97f9
mauro <mauro.castermans@gmail.com>
2023-09-20 21:37:16 +0200

    part 1 completed

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+
+  const generateRandomNumber = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+
+  const updateVotes = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+
+  const max = Math.max(...votes);
+  const index_of_max = votes.indexOf(max);
+
+  return (
+    <>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button text="vote" handleClick={updateVotes} />
+      <Button text="next anecdote" handleClick={generateRandomNumber} />
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[index_of_max]}</p>
+    </>
+  );
+};
+
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+export default App;
